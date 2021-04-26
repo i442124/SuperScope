482a5c6a7db6855d1d4f65169907e16983def1e9
Rick <442124@student.fontys.nl>
2021-04-26 21:25:23 +0200

    Update file strucutre, export files in data storage solution

--- module/storage.py
+++ module/storage.py
@@ -1,26 +1,17 @@
+import ast
 import numpy as np
 import pandas as pd
 
-GAME_DETAILS_PATH = '../data/game_details.json'
-GAME_REVIEWS_PATH = '../data/game_reviews.json'
+
+GAME_DETAILS_PATH = '../data/video-games/game-details.csv'
+GAME_REVIEWS_PATH = '../data/video-games/game-reviews.csv'
 GAME_IMAGES_PATH = '../data/box-art/small_set-256x-K500.pkl'
 
-GAME_DETAILS_SELECTION = ['title', 'platform', 'publisher', 'genres']
-GAME_REVIEWS_SELECTION = ['title', 'platform', 'meta_score', 'review_critic']
 GAME_IMAGE_SELECTION = ['title', 'platform', 'image', 'histogram', 'keypoints', 'descriptors', 'structure']
 
-games = pd.read_json(GAME_DETAILS_PATH)
-games = games[GAME_DETAILS_SELECTION]
-
-ratings = pd.read_json(GAME_REVIEWS_PATH)
-ratings = ratings[GAME_REVIEWS_SELECTION]
+games = pd.read_csv(GAME_DETAILS_PATH)
+ratings = pd.read_csv(GAME_REVIEWS_PATH)
 
 images = pd.read_pickle(GAME_IMAGES_PATH)
 images = pd.merge(games.reset_index(), images)
 images = images[GAME_IMAGE_SELECTION]
-
-ratings = ratings[ratings['review_critic'].notna()]
-ratings = ratings[ratings['meta_score'].notna()]
-
-games['genres'] = games['genres'].apply(lambda x: list(set(x)))
-games = games.drop_duplicates(subset=['title', 'platform'])
